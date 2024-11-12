a3a4118084f3cd9f9f82091237b84a3c7a08e4ea
Cristian P. <pcristian78@gmail.com>
2024-11-12 10:26:45 +0100

    Update carousel.js

--- common-js/carousel.js
+++ common-js/carousel.js
@@ -1,3 +1,3 @@
-//run: npx webpack in order to generate the bundle: /dist/carousel-bundle.js
+//run: npx webpack in order to generate the bundle: /dist/carousel-common.js
 window.CRPLCarousel = require('../src/carousel.cjs');
 import '../src/style.css';
